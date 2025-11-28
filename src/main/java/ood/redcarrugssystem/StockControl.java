4675f9c244cbcb6b26b7f68ead3a6b6d0d9ce65e
Ewan Charnley <A0096607@live.tees.ac.uk>
2025-11-28 14:41:12 +0000

    Fixed git project
    Accidentally made the github within the java project, fixed it.

---
+++ src/main/java/ood/redcarrugssystem/StockControl.java
@@ -0,0 +1,35 @@
+package ood.redcarrugssystem;
+
+/*
+ICA Group Project - Redcar Rugs System.
+- A0096607 Ewan
+- S3583123 Charan
+- S3576323 Karthik
+- S3574709 Kareem
+- S3560933 Siddhartha
+
+Current Phase: GUI.
+- Design the GUI system to the ICA specifications.
+
+Next Phase: Data.
+Total Phases left: 5.
+
+
+Main criteria:
+ - Refer to ICA for specification of the project.
+ - No use of Generative AI (disqualifies our entire project if used).
+
+
+--- CODING STANDARDS ---
+- Follow Java naming convention and OOD Module requirements
+- Do correct Indentation.
+- Establish a balanced layout.
+- *Thoroughly* comment everything in all methods, even if it is repetitive.
+*/
+
+
+public class StockControl {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
